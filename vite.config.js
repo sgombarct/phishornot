5a7ca16ca8a49a0a5a1ac8a6c30042d63bde3b75
sgombarct <53843744+sgombarct@users.noreply.github.com>
2026-02-21 21:41:04 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /\.js$/,
+  },
+})
